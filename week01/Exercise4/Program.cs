ab78c617735a83db6abfea0753ac01b305831c1c
4yume <yume84@outlook.jp>
2026-01-10 18:01:41 +0900

    list and generics assignment

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int answer = -1;
+        while (answer != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            answer = int.Parse(Console.ReadLine());
+
+            if (answer != 0)
+            {
+                numbers.Add(answer);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest is: {largest}");
     }
 }

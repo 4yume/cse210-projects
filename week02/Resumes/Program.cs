7a3e2e19e43509b4c895e5af1112ce27cb5c348e
4yume <yume84@outlook.jp>
2026-01-13 16:54:37 +0900

    learning activity Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //job class
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Developper";
+        job2._company = "Apple";
+        job2._startYear = 2025;
+        job2._endYear = 2028;
+
+
+        //Resume class
+        Resume myResume = new Resume();
+        myResume._name = "John";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

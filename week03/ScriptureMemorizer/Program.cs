f01de44eb9594292b05ebb4b0d04c719df9a6063
4yume <yume84@outlook.jp>
2026-01-23 18:02:08 +0900

    add creativity

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,14 +1,25 @@
+//add a library of scriptures rather than a single one
+//choose scriptures at random to present to the user 
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("John", 3, 16);
+        List<Scripture> library = new List<Scripture>();
 
-        string text = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+        library.Add(new Scripture(
+            new Reference("John", 3, 16),
+            "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."));
 
-        Scripture scripture = new Scripture(reference, text);
+        library.Add(new Scripture(
+            new Reference("Proverbs", 3, 5, 6),
+            "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths."));
+
+        Random random = new Random();
+        Scripture scripture = library[random.Next(library.Count)];
 
 
         while (true)
